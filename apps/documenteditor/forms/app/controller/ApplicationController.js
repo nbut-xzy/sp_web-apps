e14ea08e8aa065c2b450436054455e8690c5f493
tcb602201731 <602201731@qq.com>
2025-11-12 15:50:27 +0800

    no message

--- apps/documenteditor/forms/app/controller/ApplicationController.js
+++ apps/documenteditor/forms/app/controller/ApplicationController.js
@@ -1342,21 +1342,25 @@ define([
         },
 
 
-        insertImage: function (data) { // gateway1
+        insertImage: function (data, type) { // gateway1
             console.log("🚀 ~ data11111111111111111111:", data)
-            if (data && (data.url || data.images)) {
-                data.url && console.log("Obsolete: The 'url' parameter of the 'insertImage' method is deprecated. Please use 'images' parameter instead.");
+            if (type != 1) {
+                if (data && (data.url || data.images)) {
+                    data.url && console.log("Obsolete: The 'url' parameter of the 'insertImage' method is deprecated. Please use 'images' parameter instead.");
 
-                var arr = [];
-                if (data.images && data.images.length > 0) {
-                    for (var i = 0; i < data.images.length; i++) {
-                        data.images[i] && data.images[i].url && arr.push(data.images[i].url);
-                    }
-                } else
-                    data.url && arr.push(data.url);
-                data._urls = arr;
+                    var arr = [];
+                    if (data.images && data.images.length > 0) {
+                        for (var i = 0; i < data.images.length; i++) {
+                            data.images[i] && data.images[i].url && arr.push(data.images[i].url);
+                        }
+                    } else
+                        data.url && arr.push(data.url);
+                    data._urls = arr;
+                }
+                Common.NotificationCenter.trigger('storage:image-insert', data);
+            } else {
+                this.api.asc_InsertSignature(data.url, data.sId, data.token);
             }
-            Common.NotificationCenter.trigger('storage:image-insert', data);
         },
 
         insertImageFromStorage: function (data) {
